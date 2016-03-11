931106883db817a9036c7849465bf155beb7219d
roth141 <roth141@mail.chapman.edu>
2016-03-10 23:04:38 -0800

    honor pledge

---
+++ README.py
@@ -0,0 +1,25 @@
+# First Programming Exercises
+
+ [![License](http://img.shields.io/badge/license-MIT-blue.svg)](http://en.wikipedia.org/wiki/MIT_License)
+
+**Author:** Lydia Roth
+
+**Due date:** 3/11/16
+
+
+
+This homework involved manipulating Turtle graphics and completing for loop problems.
+
+## What I Learned from This 
+I learned how to create a star and manipulate turtle graphic
+
+## Honor Pledge
+
+I pledge that all the work in this repository is my own with only the following exceptions:
+
+* Content of starter files supplied by the instructor;
+* Code borrowed from another source, documented with correct attribution in the code and summarized here.
+
+Signed,
+
+Lydia Roth
