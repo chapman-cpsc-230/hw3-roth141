b8ebdfd5a72ed2618d79a4022fda49af43c61fbb
M. Andrew Moshier <moshier@chapman.edu>
2016-03-13 11:41:54 -0700

    Comment added by Dr M

--- Pentagon_HW4.py
+++ Pentagon_HW4.py
@@ -7,10 +7,14 @@ Created on Thu Mar  3 23:22:45 2016
 
 import turtle
 bob= turtle.Pen()
-bob.left(35)
+bob.left(35) ### MAM: What's this for?
 num_sidesinp= raw_input ("Enter number of sides:")
 num_sides = int(num_sidesinp)
-length = raw_input ("Enter length:")
+length = raw_input ("Enter length:") ## You never use this user input. 
+                                     ## So if the user asked for a bigger figure, 
+                                     ### it would still use 100
+
+
 for side in range(num_sides): 
      bob.forward(100.0)
      bob.right(360.0/num_sides)
